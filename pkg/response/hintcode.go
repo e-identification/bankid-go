543c240ffce9ca4beccd5fa3604f99f9d6b017f7
Acksell <axengstrom@gmail.com>
2025-08-02 20:37:37 +0200

    Add more hintcodes

--- pkg/response/hintcode.go
+++ pkg/response/hintcode.go
@@ -14,8 +14,18 @@ const (
 	// When the client start the may be a short delay until all ID:s are registered.
 	// The User may not have any usable ID:s at all, or has not yet inserted their smart card.
 	HintCodeStarted = HintCode("started")
+	// HintCodeUserMrtd is the hint for an order that is pending.
+	// A client has launched and received the order but additional steps
+	// for providing MRTD information is required to proceed with the order.
+	HintCodeUserMrtd = HintCode("userMrtd")
+	// Order is waiting for the user to confirm that they have received this
+	// order while in a call with your organization.
+	HintCodeUserCallConfirm = HintCode("userCallConfirm")
 	// HintCodeUserSign is the hint for an order that is pending. A client has received the order.
 	HintCodeUserSign = HintCode("userSign")
+	// HintCodeProcessing is the hint for when the order is pending. The client has received
+	// and signed the order, but signature processing is underway.
+	HintCodeProcessing = HintCode("processing")
 	// HintCodeExpiredTransaction is the hint for an order that has expired.
 	HintCodeExpiredTransaction = HintCode("expiredTransaction")
 	// HintCodeCertificateError is a hint for when the provided certificate is invalid.
@@ -26,4 +36,15 @@ const (
 	HintCodeCancelled = HintCode("cancelled")
 	// HintCodeStartFailed is a hint for when an order could not be initialized.
 	HintCodeStartFailed = HintCode("startFailed")
+	// HintCodeUserDeclinedCall is a hint for when the order was cancelled because
+	// the user indicated in the app that they are not in a call with your organization.
+	HintCodeUserDeclinedCall = HintCode("userDeclinedCall")
+	// HintCodeNotSupportedByUserApp is a hint for an order that was picked up by a
+	// client that does not support the requested feature.
+	// The BankID client used by the user needs to be updated to a later version that
+	// supports the features that are required to complete the order.
+	HintCodeNotSupportedByUserApp = HintCode("notSupportedByUserApp")
+	// HintCodeTransactionRiskBlocked is a hint for when the risk for the
+	// order was too high and the order was blocked.
+	HintCodeTransactionRiskBlocked = HintCode("transactionRiskBlocked")
 )
